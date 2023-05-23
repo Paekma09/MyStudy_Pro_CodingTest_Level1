0d5b4bf158536f434544165c76a6cdbe137bd53b
KJ Nam <paekma09@gmail.com>
2023-05-23 20:24:14 +0900

    Sum between two integers_First

---
+++ Pro_Level1_Sum between two integers/src/com/main/Main.java
@@ -0,0 +1,48 @@
+package com.main;
+
+public class Main {
+	/*
+	[문제 설명]
+	두 정수 a, b가 주어졌을 때 a와 b 사이에 속한 모든 정수의 합을 리턴하는 함수, solution을 완성하세요.
+	예를 들어 a = 3, b = 5인 경우, 3 + 4 + 5 = 12이므로 12를 리턴합니다.
+	[제한사항]
+	a와 b가 같은 경우는 둘 중 아무 수나 리턴하세요.
+	a와 b는 -10,000,000 이상 10,000,000 이하인 정수입니다.
+	a와 b의 대소관계는 정해져있지 않습니다.
+	[입출력 예]
+	a	b	return
+	3	5	12
+	3	3	3
+	5	3	12
+	*/
+	public static void main(String[] args) {
+		int a1 = 3;
+		int b1 = 5;
+		int a2 = 3;
+		int b2 = 3;
+		int a3 = 5;
+		int b3 = 3;
+		
+		System.out.print(a1+" "+b1+" ");
+		System.out.println(solution(a1, b1));
+		System.out.print(a2+" "+b2+" ");
+		System.out.println(solution(a2, b2));
+		System.out.print(a3+" "+b3+" ");
+		System.out.println(solution(a3, b3));
+	}
+	
+	public static long solution(int a, int b) {
+        long answer = 0;
+        // a와 b의 비교 분기하여 반복문으로 a와 b사이의 정수 합을 구한다.
+        if(a>b){
+            for(int i=b; i<=a; i++){
+                answer += i;
+            }
+        }else {
+            for(int i=a; i<=b; i++){
+                answer += i;
+            }
+        }
+        return answer;
+    }
+}
